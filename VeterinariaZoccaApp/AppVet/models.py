8d84b54ea023cc94cca3929ce7230e5d77b1f325
Ramiro Monserrat <ramiro1987@hotmail.com>
2022-05-02 18:22:03 -0300

    Ajustando cambios

--- VeterinariaZoccaApp/AppVet/models.py
+++ VeterinariaZoccaApp/AppVet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import DateField, DateTimeField
 
 
 # Create your models here.
@@ -8,14 +9,20 @@ class Clientes(models.Model):
     nombre= models.CharField('nombre',max_length=20) 
     apellido=models.CharField('apellido',max_length=20) 
     dni=models.IntegerField('dni')
+    
     def __str__(self) -> str:
         return f'{self.nombre} {self.apellido}'
     
        
 class Turnos(models.Model):
-    dia=models.CharField('dia',max_length=20)
-    hora=models.CharField('hora',max_length=20)
+    dia=models.DateField()
+    hora=models.DateTimeField()
     email=models.EmailField()
+    link=models.CharField(max_length=20)
+    cliente=models.ForeignKey(Clientes,on_delete=models.CASCADE)
+   
+
+    
    
     def __str__(self) -> str:
         return f'{self.dia} {self.hora}'
@@ -31,9 +38,14 @@ class Productos(models.Model):
 class Animales(models.Model):
     nombre_animal= models.CharField('nombre_animal',max_length=20)
     raza=models.CharField('raza',max_length=20)
+    link=models.CharField(max_length=20)
+    cliente=models.ForeignKey(Clientes,on_delete=models.CASCADE)
+  
 
     def __str__(self) -> str:
         return f'{self.nombre_animal} {self.raza}'
+
+
 
 class Avatar(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
